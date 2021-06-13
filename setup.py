123bc6d86fc2d601bbb6dc3fcd9df04c5a048cc4
Kristof <kristof.kovacs@telenav.com>
2021-06-13 23:04:32 +0300

    Update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setuptools.setup(
     name='selenium_browser',
-    version='0.0.5',
+    version='0.0.6',
     author='Kristóf-Attila Kovács',
     description='selenium_browser',
     long_description=long_description,
@@ -18,7 +18,7 @@ setuptools.setup(
     url='https://github.com/kkristof200/selenium_browser',
     packages=setuptools.find_packages(),
     install_requires=[
-        'k-selenium-cookies>=0.0.5',
+        'k-selenium-cookies>=0.0.4',
         'kproxy>=0.0.1',
         'noraise>=0.0.16',
         'selenium>=3.141.0',
